8cdb16f4aec8e089a2530d2b0247d2b398b7f725
sayo-07 <ayayon.1207@gmail.com>
2024-07-28 19:12:28 +0900

    I don't know this change

--- app2.js
+++ app2.js
@@ -7,8 +7,8 @@ var server = http.createServer(
     response.write('<head><meta charset="utf-8">');
     response.write('<title>Hello</title></head>');
     response.write('<body><h1>Hello Node.js!</h1>');
-    response.write('<p>This is Node.js sample page.</p>');
-    response.write('<p>これは、Node.jsのサンプルページです。</p>', 'utf8');
+    response.write('<p>This is sayo\'s first Node.js sample page.</p>');
+    response.write('<p>さよが作った初めてのNode.jsサンプルページです。</p>', 'utf8');
     response.write('</body></html>');
     response.end();
   }
